67a43afe26c94bc86aa7a9286ef531a8e0bdf2e5
Moyses Aguagallo <leo.aigaje12@gmail.com>
2020-03-05 16:10:06 -0500

    Listar notas de base de datos con Handlebars

--- src/controllers/notes.cotroller.js
+++ src/controllers/notes.cotroller.js
@@ -1,16 +1,27 @@
 const node_controller = {};
 
+//modelo de base de datos
+const Note = require('../models/Note');
+
 node_controller.render_NoteFrom = (req, res) =>{
     res.render('notes/new-note');
 }
 
-node_controller.render_CreateNewNote = (req, res) =>{
+node_controller.render_CreateNewNote = async (req, res) =>{
     //console.log(req.body); //datos del formulario
+    const { title, description } = req.body;
+
+    //guardar en base de datos
+    const new_Note = new Note({title, description});
+    //console.log(new_Note);
+    await new_Note.save();
+
     res.send('New Note');
 }
 
-node_controller.render_Notes = (req, res) =>{
-    res.send('All Notes');
+node_controller.render_Notes = async (req, res) =>{
+    const all_notes = await Note.find();
+    res.render('notes/all-notes', { all_notes });
 }
 
 node_controller.render_EditForm = (req, res) => {
